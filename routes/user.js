7ded3719de6331d27044ddc191c175e8d8aa7e2b
phongphat <phongphat.jap@gmail.com>
2025-06-20 16:38:22 +0700

    update

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,7 @@ import {
 } from "../controllers/user.js";
 // Middleare
 import { authCheck } from "../middlewares/auth.middleware.js";
+import prisma from "../config/prisma.js";
 
 const router = express.Router();
 
@@ -24,4 +25,33 @@ router.get("/getme", authCheck, getMe);
 router.get("/user", readUser);
 router.post("/user", createUser);
 // Export
+
+router.post("/landmark", async (req, res) => {
+  try {
+    const { title, lat, lng } = req.body;
+    console.log(title, lat, lng);
+
+    const landmark = await prisma.landmark.create({
+      data: {
+        title: title,
+        lat: Number(lat),
+        lng: Number(lng),
+      },
+    });
+    console.log(landmark);
+    res.json({ message: "Create Landmark Success!!!" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/landmarks", async (req, res, next) => {
+  try {
+    const landmarks = await prisma.landmark.findMany({});
+    res.json({ result: landmarks });
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
